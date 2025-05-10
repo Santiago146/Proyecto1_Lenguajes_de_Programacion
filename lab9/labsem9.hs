4a7f40ec161632e8819f482a1dba8e304ba0e782
KevinOrl <kevnunez@estudiantec.cr>
2025-05-10 14:32:08 -0600

    Se añade la resolución del ejercicio 3 completa

--- lab9/labsem9.hs
+++ lab9/labsem9.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.List (find)
 
 -- Ejercicio 2: sumaCuadradosLista
 sumaCuadradosLista :: [Int] -> Int
@@ -12,6 +13,7 @@ cuentaPares xs = length (filter even xs)
 paresMenoresA20 :: [Int]
 paresMenoresA20 = [x | x <- [1..20], (\n -> n `mod` 2 == 0) x] -- profe no me acuerdo si habia que incluir el 20 :(
 
+-- Ejercicio 2: menuEjercicio2
 menuEjercicio2 :: IO ()
 menuEjercicio2 = do
     putStrLn "=== Menu de ejercicio 2 ==="
@@ -32,7 +34,8 @@ menuEjercicio2 = do
             putStrLn "Ingrese los numeros separados por espacios:"
             entrada <- getLine
             let numeros = map read (words entrada) :: [Int]
-            print (sumaCuadradosLista numeros)
+            putStrLn("La suma de los cuadrados de los números ingresados es: ")
+            print(sumaCuadradosLista numeros)
             menuEjercicio2
         "3" -> do
             putStrLn "Ingrese los numeros separados por espacios:"
@@ -50,6 +53,93 @@ menuEjercicio2 = do
             putStrLn "Opcion invalida"
             menuEjercicio2
 
+-- Ejercicio 3: estructura de contacto
+data Contacto = Contacto {
+    nombre :: String,
+    telefono :: String,
+    correo :: String,
+    edad :: Int
+} deriving (Show)
+
+
+type Contactos = [Contacto]
+
+--Agregar contacto
+agregarContacto :: Contacto -> Contactos -> Contactos
+agregarContacto nuevo contactoLista = 
+    case find (\c -> nombre c == nombre nuevo) contactoLista of
+        Just _  -> contactoLista
+        Nothing -> nuevo : contactoLista
+
+--Buscar Telefono
+buscarTelefono :: String -> Contactos -> String
+buscarTelefono nombreContacto contactos = 
+    case find (\c -> nombre c == nombreContacto) contactos of
+        Just contacto -> "Telefono de " ++ nombreContacto ++ ": " ++ telefono contacto
+        Nothing       -> "Contacto no encontrado"
+
+--Ver contactos
+verContactos :: Contactos -> IO ()
+verContactos [] = putStrLn "No hay contactos"
+verContactos contactos = do
+    putStrLn "Lista de contactos:"
+    mapM_ (\c -> putStrLn (nombre c ++ " - " ++ telefono c ++ " - " ++ correo c ++ " - " ++ show (edad c))) contactos
+
+--Eliminar contacto
+eliminarContacto :: String -> Contactos -> Contactos
+eliminarContacto nombreContacto contactos = 
+    filter (\c -> nombre c /= nombreContacto) contactos
+
+-- menuEjercicio3
+menuEjercicio3 :: Contactos -> IO ()
+menuEjercicio3 listaContactos = do
+    putStrLn "=== Menu de ejercicio 3 ==="
+    putStrLn "1. Agregar contacto"
+    putStrLn "2. Buscar teléfono"
+    putStrLn "3. Ver Todos los contactos"
+    putStrLn "4. Eliminar contacto"
+    putStrLn "5. Volver al menu principal"
+    putStrLn "Seleccione una opcion: "
+    opcionEjercicio2 <- getLine
+    case opcionEjercicio2 of
+        "1" -> do
+            putStrLn "Ingrese su nombre:"
+            nombre <- getLine
+            putStrLn "Ingrese su número de teléfono:"
+            telefono <- getLine
+            putStrLn "Ingrese su correo electrónico:"
+            correo <- getLine
+            putStrLn "Ingrese su edad:"
+            edadStr <- getLine
+            let edad = read edadStr :: Int
+            let nuevoContacto = Contacto nombre telefono correo edad
+            let nuevaLista = agregarContacto nuevoContacto listaContactos  -- Nombre diferente
+            putStrLn ("Contacto agregado: " ++ nombre)
+            verContactos nuevaLista
+            menuEjercicio3 nuevaLista
+        "2" -> do
+            putStrLn "Ingrese el nombre del contacto a buscar:"
+            nombreContacto <- getLine
+            let telefonoEncontrado = buscarTelefono nombreContacto listaContactos
+            putStrLn (telefonoEncontrado)
+            menuEjercicio3 listaContactos   
+        "3" -> do
+            verContactos listaContactos
+            menuEjercicio3 listaContactos 
+        "4" -> do
+            putStrLn "Ingrese el nombre del contacto a eliminar:"
+            nombreContacto <- getLine
+            let nuevaLista = eliminarContacto nombreContacto listaContactos  -- Nombre diferente
+            putStrLn ("Contacto eliminado: " ++ nombreContacto)
+            verContactos nuevaLista
+            menuEjercicio3 nuevaLista
+        "5" -> do
+            putStrLn "Volviendo al menu principal..."
+            menu
+        _   -> do
+            putStrLn "Opcion invalida"
+            menuEjercicio3 listaContactos
+
 menu :: IO ()
 menu = do
     putStrLn "=== Menu principal ==="
@@ -62,8 +152,17 @@ menu = do
         "1" -> do
             menuEjercicio2
         "2" -> do
-            menu
-        "3" -> putStrLn "Cerrando el programa..."
+            let contactosIniciales :: Contactos
+                contacto1 = Contacto "Kevin" "86454511" "kevin0506@gmail.com" 25
+                contacto2 = Contacto "Yeri" "88451236" "yeri@gmail.com" 24
+                contacto3 = Contacto "Roy" "87896541" "roy@gmail.com" 26
+                contacto4 = Contacto "Bryan" "80014587" "bryan@gmail.com" 23
+                contacto5 = Contacto "Marlen" "85874125" "marlen@gmail.com" 27
+                contactosIniciales = [contacto1, contacto2, contacto3, contacto4, contacto5]
+            menuEjercicio3 contactosIniciales
+        "3" -> do
+            putStrLn "Saliendo..."
+            return ()
         _   -> do
             putStrLn "Opcion invalida"
             menu
